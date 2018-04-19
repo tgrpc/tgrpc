c1c5ea6b50e2b3a7dac14d136ff20ae803426f44
toukii <toukii@yahoo.com>
2018-04-19 19:13:05 +0800

    init

--- tg/config.go
+++ tg/config.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"time"
+
+	"github.com/BurntSushi/toml"
 	"github.com/tgrpc/tgrpc"
+	"github.com/toukii/bytes"
+	"github.com/toukii/goutils"
 )
 
 type Rpc struct {
@@ -12,3 +17,52 @@ type Rpc struct {
 }
 
 type TG map[string]*Rpc
+
+func Setup() TG {
+	var rs TG
+	_, err := toml.DecodeFile(conf, &rs)
+	if err != nil {
+		log.Error(err)
+	}
+	return rs
+}
+
+func initSetup() {
+	rpc := Rpc{
+		Tgr: &tgrpc.Tgrpc{
+			Address:        "localhost:2080",
+			KeepaliveTime:  &tgrpc.Duration{time.Second * 100},
+			ReuseDesc:      true,
+			ProtoBasePath:  "$GOPATH/src/github.com/tgrpc/ngrpc",
+			IncludeImports: "helloworld/helloworld.proto",
+		},
+		Invokes: []*tgrpc.Invoke{
+			&tgrpc.Invoke{
+				Method:   "helloworld.Greeter/SayHello",
+				Headers:  []string{"customerId:123", "region:UK"},
+				Data:     `{"name":"tgrpc-tg1"}`,
+				N:        5,
+				Interval: &tgrpc.Ms{time.Millisecond * 200},
+				Resp: &tgrpc.Resp{
+					Cost: &tgrpc.Ms{time.Millisecond * 500},
+					Json: map[string]interface{}{
+						`message`: "Hello tgrpc-tg1",
+					},
+				},
+			},
+		},
+		Exced:    true,
+		LogLevel: "debug",
+	}
+
+	wr := bytes.NewWriter(make([]byte, 0, 256))
+	tgrs := TG{
+		"Greeter": &rpc,
+	}
+	err := toml.NewEncoder(wr).Encode(tgrs)
+	log.Infof("encode:\n%s\nerr: %+v", wr.Bytes(), err)
+	err = goutils.SafeWriteFile("tgrpc.toml", wr.Bytes())
+	if err != nil {
+		log.Error(err)
+	}
+}
--- tg/main.go
+++ tg/main.go
@@ -5,11 +5,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/BurntSushi/toml"
 	"github.com/sirupsen/logrus"
 	"github.com/tgrpc/tgrpc"
-	"github.com/toukii/bytes"
-	"github.com/toukii/goutils"
 )
 
 var (
@@ -46,7 +43,7 @@ func main() {
 
 	tg := Setup()
 	if len(tg) <= 0 {
-		log.Errorf("config is nil")
+		log.Errorf("config is nil, init: tg -i")
 	}
 	for k, tgr := range tg {
 		log.WithField("tgrpc", k).Infof("exced:%t", tgr.Exced)
@@ -75,53 +72,4 @@ func main() {
 			sg.Wait()
 		}
 	}
-}
-
-func initSetup() {
-	rpc := Rpc{
-		Tgr: &tgrpc.Tgrpc{
-			Address:        "localhost:2080",
-			KeepaliveTime:  &tgrpc.Duration{time.Second * 100},
-			ReuseDesc:      true,
-			ProtoBasePath:  "$GOPATH/src/github.com/tgrpc/ngrpc",
-			IncludeImports: "helloworld/helloworld.proto",
-		},
-		Invokes: []*tgrpc.Invoke{
-			&tgrpc.Invoke{
-				Method:   "helloworld.Greeter/SayHello",
-				Headers:  []string{"customerId:123", "region:UK"},
-				Data:     `{"name":"tgrpc-tg1"}`,
-				N:        5,
-				Interval: &tgrpc.Ms{time.Millisecond * 200},
-				Resp: &tgrpc.Resp{
-					Cost: &tgrpc.Ms{time.Millisecond * 500},
-					Json: map[string]interface{}{
-						`message`: "Hello tgrpc-tg1",
-					},
-				},
-			},
-		},
-		Exced:    true,
-		LogLevel: "debug",
-	}
-
-	wr := bytes.NewWriter(make([]byte, 0, 256))
-	tgrs := TG{
-		"Greeter": &rpc,
-	}
-	err := toml.NewEncoder(wr).Encode(tgrs)
-	log.Infof("encode:\n%s\nerr: %+v", wr.Bytes(), err)
-	err = goutils.SafeWriteFile("tgrpc.toml", wr.Bytes())
-	if err != nil {
-		log.Error(err)
-	}
-}
-
-func Setup() TG {
-	var rs TG
-	_, err := toml.DecodeFile(conf, &rs)
-	if err != nil {
-		log.Panic(err)
-	}
-	return rs
 }
