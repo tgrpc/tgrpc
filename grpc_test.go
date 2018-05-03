660a1df4937c383f6f55ed19448c361200048c39
toukii <toukii@yahoo.com>
2018-05-03 22:45:39 +0800

    test

--- grpc_test.go
+++ grpc_test.go
@@ -12,7 +12,7 @@ func init() {
 func TestInvokeGRpcGreeter(t *testing.T) {
 	tg := &Tgrpc{
 		Address:        "localhost:2080",
-		KeepaliveTime:  &Duration{time.Second * 100},
+		KeepaliveTime:  &Second{time.Second * 100},
 		ProtoBasePath:  "$GOPATH/src/github.com/tgrpc/ngrpc",
 		IncludeImports: "helloworld/helloworld.proto",
 	}
