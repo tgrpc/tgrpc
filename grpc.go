69fd41bd84b78c0430714c1922391ed9afb1b4e0
toukii <toukii@yahoo.com>
2019-03-21 10:45:12 +0800

    merge

--- grpc.go
+++ grpc.go
@@ -192,6 +192,7 @@ func (t *Tgrpc) Tocurl(ivk *Invoke, data string) string {
 	for _, h := range ivk.Headers {
 		buf.WriteString(fmt.Sprintf(" -H '%s'", h))
 	}
+	buf.WriteString(" -X 'POST' -H 'Content-Type: application/json'")
 	// buf.WriteString(` -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36' -H 'Content-Type: text/plain;charset:utf-8' -H 'Accept: /'`)
 	buf.WriteString(fmt.Sprintf(" --data-binary '%s'", data))
 	buf.WriteString(" --compressed")
